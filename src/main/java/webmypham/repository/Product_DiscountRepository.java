96c98b9681e5c5310803435c6a9edc3d090c21dc
hoangtrinh050499 <hoangvantrinh99@gmail.com>
2021-07-06 15:06:48 +0700

    fix discount

--- src/main/java/webmypham/repository/Product_DiscountRepository.java
+++ src/main/java/webmypham/repository/Product_DiscountRepository.java
@@ -14,7 +14,7 @@ public interface Product_DiscountRepository extends JpaRepository<Product_Discou
 //	@Query(value ="SELECT * from product_discount WHERE end >= '?1'",nativeQuery = true)
 //	List<Product_Discount> findlistdiscount(LocalDate date);
 	
-	@Query(value ="SELECT level from discount d INNER join product_discount pd on d.id = pd.iddiscount INNER JOIN products p on pd.idproduct = ?1 WHERE pd.start<=?2 and pd.end >= ?2 LIMIT 1",nativeQuery = true)
+	@Query(value ="SELECT level from discount d INNER join product_discount pd on d.id = pd.iddiscount INNER JOIN products p on pd.idproduct = ?1 WHERE pd.start<='?2' and pd.end >= '?2' LIMIT 1",nativeQuery = true)
 	Integer finddiscount(int id,String date);
 	
 }
